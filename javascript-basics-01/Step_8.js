c7c4979a711303a9b3037de7c5385a13ed4d7d1f
Yahya Darwich <ydarwich18@gmail.com>
2021-11-25 15:37:29 +0200

    Step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,8 @@
+function checkAge() {
+  var age = document.getElementById("age").value;
+  if (age > 18) {
+    document.write("you are over 18");
+  } else {
+    document.write("you are under 18");
+  }
+}
