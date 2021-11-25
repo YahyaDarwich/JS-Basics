1fb88b8bfcd8a25e3a07be296581e84cc25e3e41
Yahya Darwich <ydarwich18@gmail.com>
2021-11-25 15:34:05 +0200

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+function get_information() {
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+  alert("Nom : " + name + "\nprénom : " + surname + "\nVille : " + city);
+}
