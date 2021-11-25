1fa1c77b64ef7219783218aa73c84aa708b099b6
Yahya Darwich <ydarwich18@gmail.com>
2021-11-25 15:34:34 +0200

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+function get_multiply() {
+  var firstNumber, secondNumber;
+  firstNumber = document.getElementById("first_number").value;
+  secondNumber = document.getElementById("second_number").value;
+  document.write(
+    "<br/>The Multiply of numbers is:" + firstNumber * secondNumber
+  );
+}
