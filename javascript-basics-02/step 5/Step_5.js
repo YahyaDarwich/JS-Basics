5e6bb476b9bfdfad6b638db42dcf510605dc2652
Yahya Darwich <ydarwich18@gmail.com>
2021-11-27 19:51:24 +0200

    Step_5

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,13 @@
+function over(x) {
+  if (x.id == "image1") {
+    x.src = "images/image1_2.jpg";
+  } else if (x.id == "image2") {
+    x.src = "images/image2_2.jpg";
+  } else if (x.id == "image3") {
+    x.src = "images/image3_2.jpg";
+  } else if (x.id == "image4") {
+    x.src = "images/image4_2.jpg";
+  } else {
+    x.src = "images/image5_2.jpg";
+  }
+}
