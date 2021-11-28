763dec35c34c1c9e24f0220fb27bf8ee3f39ba8a
Yahya Darwich <ydarwich18@gmail.com>
2021-11-28 15:07:43 +0200

    Step_1

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,6 @@
+function addBorder1(){
+    document.querySelector("img").style.border = "3px solid red";
+}
+function addBorder2(){
+    document.getElementById("image2").style.border = "3px solid red";
+}
